d666b7b4eeb7f54ae113c9c4bbeabc2e4ad24338
Priyam Gupta <priyammgupta@gmail.com>
2017-11-06 10:57:03 +0530

    lession-2-completed

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,7 +20,12 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+predictions = X*theta;
+ex = sigmoid(predictions);
+logResult = -y.*log(ex) - (1-y).*log(1-ex);
+J = (1/m)* sum(logResult);
 
+grad = (1/m)*sum((ex - y).*X);
 
 
 
